a65a95018315b960dbfb49f8b7341343fc10a528
Paresh Sharma <Paresh.Sharma@Valaries-MBP.hsd1.il.comcast.net>
2020-01-15 11:32:26 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,21 +3,30 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[2];
 
-cardsInPlay.push(cards[2]);
-
-
-let cardTwo = cards[3];
-
-cardsInPlay.push(cards[3]);
-
-console.log(cardsInPlay);
-
-if (cardsInPlay.length === 2 ) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert ("You found a match");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1])  {
+		console.log("You have found a match");
 	} else {
-		alert ("Sorry try again");
+		console.log("Sorry try again");
 	}
 }
+
+
+function flipCard (cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push[cards[cardId]];
+
+}
+
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+
+
+
+
