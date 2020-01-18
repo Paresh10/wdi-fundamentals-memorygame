86b0424e581743a29fcfb268ec269d9463702a12
Paresh Sharma <Paresh.Sharma@Valaries-MBP.hsd1.il.comcast.net>
2020-01-17 19:39:58 -0600

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,31 @@
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
 
 let cardsInPlay = [];
-
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1])  {
@@ -14,17 +37,22 @@ function checkForMatch() {
 
 
 function flipCard (cardId) {
+	// cardsInPlay.push[cards[cardId]].rank;
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-	cardsInPlay.push[cards[cardId]];
+	// checkForMatch();
 
 }
 
 
+
+
 flipCard(0);
 flipCard(2);
-checkForMatch();
+
 
 
 
