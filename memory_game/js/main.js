9c583a8ae643e458372145c1545a507c7df91c68
Paresh Sharma <Paresh.Sharma@Valaries-MBP.hsd1.il.comcast.net>
2020-01-19 14:12:03 -0600

    Modiefied index.html and new file gitignore

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,31 +27,45 @@ let cards = [
 
 let cardsInPlay = [];
 
+
 function checkForMatch() {
+
 	if (cardsInPlay[0] === cardsInPlay[1])  {
-		console.log("You have found a match");
+		alert("You have found a match");
 	} else {
-		console.log("Sorry try again");
+		alert("Sorry try again");
 	}
 }
 
 
-function flipCard (cardId) {
-	// cardsInPlay.push[cards[cardId]].rank;
 
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+function flipCard () {
 
+	const cardId = this.getAttribute('data-id');
+	this.setAttribute("src", cards[cardId].cardImage);	
+
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch ();
 
 }
 
 
 
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+
+  const cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+		
+	}
+}
+
+createBoard ();
+
 
 
 
